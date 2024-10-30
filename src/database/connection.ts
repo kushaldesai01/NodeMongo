82a9ab202626908eb53d0af94a898fe54cf112cf
kushal <kushaldesai01@gmail.com>
2024-10-30 12:23:44 +0530

    unknown error type

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { APP } from "../variables/constants";
+import { getErrorMessage } from "../services/functions";
 
 export const connectToDatabase = async () => {
   try {
     await mongoose.connect(APP.DATABASE_URL);
     console.log("Connected to database");
-  } catch (error: any) {
-    console.log(error.message);
+  } catch (error: unknown) {
+    console.log(getErrorMessage(error));
   }
 };
